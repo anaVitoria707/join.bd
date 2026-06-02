8c9475996bda8ceb4b3e006af8f1df8ee29d951c
anaVitoria707 <anavalente3010@gmail.com>
2026-06-02 20:26:50 -0300

    Update SQL queries for book loans and sectors

--- join.bd.sql
+++ join.bd.sql
@@ -1,1 +1,9 @@
- f.nome f.cargo
+Select l.titulo, l.autor AS livro, 
+e.nome_aluno, e.data_emprestimo 
+FROM emprestimos e INNER JOIN 
+livros l ON e.id_livro = l.id
+
+Select s.nome_setor, s.andar AS setores, 
+ f.nome, f.cargo
+FROM setores s LEFT JOIN 
+funcionarios f ON s.id_funcionario = f.id;
